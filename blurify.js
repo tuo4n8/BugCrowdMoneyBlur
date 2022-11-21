6e0932f75346671e120ffb738a56fbf574693ebf
@n <ttuongann@gmail.com>
2022-11-21 10:40:24 +0700

    asd

--- blurify.js
+++ blurify.js
@@ -1,8 +1,19 @@
 payments = document.querySelectorAll(".bc-reward")
 
+payments1 = document.querySelectorAll(".bc-stat__fig")
+
+
 payments.forEach(payment =>{
 	// fake money
 	// payment.innerHTML = "$1"
-	//
-	payment.style.filter = "blur(5px)"
+	
+	payment.style.filter = "blur(10px)"
+})
+
+
+payments1.forEach(payment =>{
+	// fake money
+	// payment.innerHTML = "$1"
+	
+	payment.style.filter = "blur(10px)"
 })
--- unblurify.js
+++ unblurify.js
@@ -1,6 +1,16 @@
 payments = document.querySelectorAll(".bc-reward")
 
+payments1 = document.querySelectorAll(".bc-stat__fig")
+
 payments.forEach(payment =>{
+	// fake money
+	// payment.innerHTML = "$1"
+	//
+	payment.style.filter = "blur(0px)"
+})
+
+
+payments1.forEach(payment =>{
 	// fake money
 	// payment.innerHTML = "$1"
 	//
