56e85272e837f6588783397c34d140c6d3bae0ed
tuo4n8 <ttuongann@gmail.com>
2024-09-20 12:08:14 +0700

    fix some bug

--- blurify.js
+++ blurify.js
@@ -2,11 +2,17 @@ payments = document.querySelectorAll(".bc-reward")
 
 payments1 = document.querySelectorAll(".bc-stat__fig")
 
-spans = document.querySelectorAll('.spec-amount-in-currency span');
-
-strongs = document.querySelectorAll('.spec-hacktivity-item-bounty');
-
 totals_bugcrowd = document.querySelectorAll('#content > div.cc-100vh-container > main > div > section > div > div.tk-sidebar.col-md-3 > div:nth-child(1) > div > p:nth-child(1)')
+
+totals_bugcrowd_page = document.querySelectorAll('.bc-panel__main strong, .bc-panel__main small');
+
+
+// hacerkobne
+spans = document.querySelectorAll('.spec-amount-in-currency span');
+strongs = document.querySelectorAll('.spec-hacktivity-item-bounty');
+cardContent = document.querySelector('.spec-user-payouts')
+moneyElements = document.querySelectorAll('.card__content h4:nth-child(2)');
+
 
 
 payments.forEach(payment =>{
@@ -16,6 +22,9 @@ payments.forEach(payment =>{
 	payment.style.filter = "blur(10px)"
 })
 
+totals_bugcrowd_page.forEach(element => {
+    element.style.filter = 'blur(10px)';
+});
 
 totals_bugcrowd.forEach(total =>{
 	// fake money
@@ -37,6 +46,13 @@ spans.forEach(span => {
 });
 
 
+moneyElements.forEach(element => {
+    element.style.filter = 'blur(10px)';
+});
+
+cardContent.forEach(element => {
+    element.style.filter = 'blur(10px)';
+});
 
 
 strongs.forEach(strong => {
--- unblurify.js
+++ unblurify.js
@@ -1,13 +1,21 @@
 //bugcrowd
 payments = document.querySelectorAll(".bc-reward")
 
+total_bc_page = document.querySelectorAll('.bc-panel__main strong, .bc-panel__main small');
+
 payments1 = document.querySelectorAll(".bc-stat__fig")
+
+totals_bugcrowd_page = document.querySelectorAll('.bc-panel__main strong, .bc-panel__main small');
 
 spans = document.querySelectorAll('.spec-amount-in-currency span');
 
 strongs = document.querySelectorAll('.spec-hacktivity-item-bounty');
 
 totals_bugcrowd = document.querySelectorAll('#content > div.cc-100vh-container > main > div > section > div > div.tk-sidebar.col-md-3 > div:nth-child(1) > div > p:nth-child(1)')
+
+cardContent = document.querySelector('.spec-user-payouts')
+
+moneyElements = document.querySelectorAll('.card__content h4:nth-child(2)');
 
 payments.forEach(payment =>{
 	// fake money
@@ -17,12 +25,17 @@ payments.forEach(payment =>{
 })
 
 
+totals_bugcrowd_page.forEach(element => {
+    element.style.filter = 'blur(0px)';
+});
+
 totals_bugcrowd.forEach(total =>{
 	// fake money
 	// payment.innerHTML = "$1"
 	
 	total.style.filter = "blur(0px)"
 })
+
 
 payments1.forEach(payment =>{
 	// fake money
@@ -37,8 +50,19 @@ spans.forEach(span => {
 	span.style.filter = 'blur(0px)';
 });
 
+moneyElements.forEach(element => {
+    element.style.filter = 'blur(0px)';
+});
 
 
 strongs.forEach(strong => {
 	strong.style.filter = 'blur(0px)';
+});
+
+cardContent.forEach(element => {
+    element.style.filter = 'blur(0px)';
+});
+
+total_bc_page.forEach(element => {
+    element.style.filter = 'blur(0px)';
 });
